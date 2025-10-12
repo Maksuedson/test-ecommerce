006eb4227bf9f934167b5413b0aeb859b9635719
Maksuedson <maksuedson@hotmail.com>
2025-10-12 16:17:19 -0300

    Criado classe Produto

---
+++ src/main/java/com/ecommerce/ecommerce/entity/Produto.java
@@ -0,0 +1,18 @@
+package com.ecommerce.ecommerce.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Produto {
+
+    private UUID id;
+    private String nome;
+    private BigDecimal preco;
+    private Categoria categoria;
+    private BigDecimal estoque;
+    private LocalDateTime dataCadastro;
+    private LocalDateTime dataUltimaModificacao;
+
+
+}
