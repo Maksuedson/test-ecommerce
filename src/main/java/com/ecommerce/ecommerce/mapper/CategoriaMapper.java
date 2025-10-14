7534f7080b59c614e91066fa136de13a1e0da143
Maksuedson <maksuedson@hotmail.com>
2025-10-14 17:51:28 -0300

    Criado Classe CategoriaMapper

---
+++ src/main/java/com/ecommerce/ecommerce/mapper/CategoriaMapper.java
@@ -0,0 +1,36 @@
+package com.ecommerce.ecommerce.mapper;
+
+import com.ecommerce.ecommerce.controller.response.CategoriaResponse;
+import com.ecommerce.ecommerce.dto.CategoriaDto;
+import com.ecommerce.ecommerce.entity.Categoria;
+
+public class CategoriaMapper {
+
+    public static Categoria mapToCategoria(CategoriaDto categoriaDto){
+
+        return new Categoria(
+                categoriaDto.getId(),
+                categoriaDto.getNome(),
+                categoriaDto.getProdutos(),
+                categoriaDto.getDataCadastro(),
+                categoriaDto.getDataUltimaModificacao());
+    }
+
+    public static CategoriaDto mapToCategoriaDto(Categoria categoria){
+
+        return new CategoriaDto(
+                categoria.getId(),
+                categoria.getNome(),
+                categoria.getProdutos(),
+                categoria.getDataCadastro(),
+                categoria.getDataUltimaModificacao());
+    }
+
+    public static CategoriaResponse mapToCategoriaResponse(CategoriaDto categoriaDto){
+
+        return new CategoriaResponse(
+                categoriaDto.getId(),
+                categoriaDto.getNome(),
+                categoriaDto.getDataCadastro());
+    }
+}
