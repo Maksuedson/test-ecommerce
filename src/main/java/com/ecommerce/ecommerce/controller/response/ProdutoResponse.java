eadba4cb6123d471103656c3feac2a449d58c87b
Maksuedson <maksuedson@hotmail.com>
2025-10-14 15:34:08 -0300

    Criado Classe ProdutoResponse

---
+++ src/main/java/com/ecommerce/ecommerce/controller/response/ProdutoResponse.java
@@ -0,0 +1,24 @@
+package com.ecommerce.ecommerce.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProdutoResponse {
+
+	private UUID id;
+	private String nome;
+	private String codigoBarras;
+	private BigDecimal estoque;
+	private BigDecimal preco;
+	private LocalDateTime dataCadastro;
+}
