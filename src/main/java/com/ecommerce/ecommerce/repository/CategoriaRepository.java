b9bfa01c12d1254ac0a828468a418586abb63c7e
Maksuedson <maksuedson@hotmail.com>
2025-10-14 17:21:29 -0300

    Criado classe CategoriaRepository

--- src/main/java/com/ecommerce/ecommerce/repository/CategoriaRepository.java
+++ src/main/java/com/ecommerce/ecommerce/repository/CategoriaRepository.java
@@ -3,6 +3,10 @@ package com.ecommerce.ecommerce.repository;
 import com.ecommerce.ecommerce.entity.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+    List<Categoria> findByNomeContaining(String nome);
 
 }
