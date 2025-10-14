5bc38abe40a90ae11a0f38a296fb4fdd53f99947
Maksuedson <maksuedson@hotmail.com>
2025-10-14 09:53:15 -0300

    Criado query jpql

--- src/main/java/com/ecommerce/ecommerce/repository/ClienteRepository.java
+++ src/main/java/com/ecommerce/ecommerce/repository/ClienteRepository.java
@@ -3,7 +3,13 @@ package com.ecommerce.ecommerce.repository;
 import com.ecommerce.ecommerce.entity.Cliente;
 import com.ecommerce.ecommerce.entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+
+    @Query("select c from Cliente c where c.nome like %?1%")
+    List<Cliente> findByName(String nome);
     
 }
