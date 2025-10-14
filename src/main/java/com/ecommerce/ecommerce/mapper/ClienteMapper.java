dd69f338d850c7452963e9ecad69cea1ab736368
Maksuedson <maksuedson@hotmail.com>
2025-10-14 09:51:06 -0300

    Criado cliente mapper

---
+++ src/main/java/com/ecommerce/ecommerce/mapper/ClienteMapper.java
@@ -0,0 +1,48 @@
+package com.ecommerce.ecommerce.mapper;
+
+import com.ecommerce.ecommerce.dto.ClienteDto;
+import com.ecommerce.ecommerce.entity.Cliente;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ClienteMapper {
+
+	public static Cliente mapToCliente(ClienteDto clienteDto) {
+		return new Cliente(
+				clienteDto.getId(),
+				clienteDto.getTenantId(),
+				clienteDto.getNome(),
+				clienteDto.getEndereco(),
+				clienteDto.getNumero(),
+				clienteDto.getBairro(),
+				clienteDto.getCep(),
+				clienteDto.getCidade(),
+				clienteDto.getEstado(),
+				clienteDto.getTelefone(),
+				clienteDto.getCelular(),
+				clienteDto.getEmail(),
+				clienteDto.getDataCadastro(),
+				clienteDto.getDataUltimaModificacao()
+				);
+	}
+	
+	public static ClienteDto mapToClienteDto(Cliente cliente) {
+		return new ClienteDto(
+				cliente.getId(),
+				cliente.getTenantId(),
+				cliente.getNome(),
+				cliente.getEndereco(),
+				cliente.getNumero(),
+				cliente.getBairro(),
+				cliente.getCep(),
+				cliente.getCidade(),
+				cliente.getEstado(),
+				cliente.getTelefone(),
+				cliente.getCelular(),
+				cliente.getEmail(),
+				cliente.getDataCadastro(),
+				cliente.getDataUltimaModificacao()
+				);
+	}
+}
+;
